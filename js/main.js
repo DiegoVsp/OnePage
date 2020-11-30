4fcd9855e0127c94ae1b63259e5fc1c4bfe784b7
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-11-30 15:03:08 -0300

    Projeto Finalizado!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-(function($){
+(function ($) {
 
   "use strict";
   // inicializar nosso contador
   $('.contar').counterUp({
-    time:1000,
-    delay:10
+    time: 1000,
+    delay: 10
+  });
+  
+  //Efeito Scroll 
+  var $doc = $('html, body');
+  $('.scroll-page').click(function () {
+    $doc.animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
   });
 })(jQuery);
